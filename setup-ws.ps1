110c27b4972e4f40c85702e13350b80101c3e0db
Josh Schlesinger <josh@jschlesinger.net>
2017-08-25 09:16:53 -0500

    import

---
+++ setup-ws.ps1
@@ -0,0 +1,15 @@
+﻿Install-PackageProvider -Name NuGet -Force
+Install-PackageProvider -Name PowerShellGet -Force
+Install-PackageProvider ChocolateyGet
+
+Install-Module posh-git -Force
+Install-Module posh-ssh -Force
+
+Install-Package -ProviderName ChocolateyGet -Name git -Force
+Install-Package -ProviderName ChocolateyGet -Name gitkraken -Force
+Install-Package -ProviderName ChocolateyGet -Name 7zip -Force
+Install-Package -ProviderName ChocolateyGet -Name jdk8 -Force
+Install-Package -ProviderName ChocolateyGet -Name nodejs.install -Force
+Install-Package -ProviderName ChocolateyGet -Name nodejs.install -Force
+Install-Package -ProviderName ChocolateyGet -Name notepad2 -Force
+Install-Package -ProviderName ChocolateyGet -Name visualstudiocode -Force
